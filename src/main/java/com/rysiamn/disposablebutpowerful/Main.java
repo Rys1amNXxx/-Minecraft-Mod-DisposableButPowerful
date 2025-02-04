c2c8307e2a6a25f7a4a9fae4e5a8461424dbe948
ASUS <1282439578@qq.com>
2025-02-04 14:22:08 +0000

    OneTimeKillSword

--- src/main/java/com/rysiamn/disposablebutpowerful/Main.java
+++ src/main/java/com/rysiamn/disposablebutpowerful/Main.java
@@ -1,19 +1,50 @@
 package com.rysiamn.disposablebutpowerful;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.entity.vehicle.MinecartFurnace;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.MinecartItem;
+import net.minecraft.world.item.Rarity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
+
+
 @Mod(Main.MODID)
-public class Main
-{
+public class Main {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "disposablebutpowerful";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public Main()
-    {
+    public Main() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        Items.ITEMS.register(modEventBus);
+
+        tabs.register(modEventBus);
+
+        modEventBus.addListener(this::addCreative);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(Items.ONE_TIME_KILL_SWORD);
+        }
+    }
+
+    public static class Items {
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+        public static final RegistryObject<Item> ONE_TIME_KILL_SWORD = ITEMS.register("one_time_kill_sword",
+                () -> new OneTimeKillSword(new Item.Properties().durability(1).rarity(Rarity.EPIC)));
     }
 }
---
+++ src/main/java/com/rysiamn/disposablebutpowerful/OneTimeKillSword.java
@@ -0,0 +1,24 @@
+package com.rysiamn.disposablebutpowerful;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
+
+public class OneTimeKillSword extends SwordItem {
+
+    public OneTimeKillSword(Item.Properties properties) {
+        super(Tiers.NETHERITE, 999, -2.4F, properties);
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public boolean isRepairable(ItemStack stack) {
+        return false;
+    }
+
+}
